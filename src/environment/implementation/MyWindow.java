940991a33daca513ce7e14a311003cd51f871b5e
Tom Kiesow <tomkiesow123@gmail.com>
2017-02-19 11:32:41 +0100

    Unichtbarer Cursor

--- src/environment/implementation/MyWindow.java
+++ src/environment/implementation/MyWindow.java
@@ -1,7 +1,10 @@
 package environment.implementation;
 
 import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Point;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,15 +12,14 @@ import javax.swing.JPanel;
 import environment.model.Window;
 
 public class MyWindow implements Window {
-	
+
 	private static final Window INSTANCE = new MyWindow();
 
 	protected final JFrame FRAME = new JFrame();
 	protected final JPanel PANEL = new JPanel();
 
-
 	private MyWindow() {
-		
+
 		this.FRAME.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.FRAME.setUndecorated(true);
 		this.FRAME.setSize(Toolkit.getDefaultToolkit().getScreenSize());
@@ -25,14 +27,18 @@ public class MyWindow implements Window {
 
 		int offsetX, offsetY;
 		int width, height;
-		
+
 		offsetX = ((int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() - 1024) >> 1);
 		offsetY = ((int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() - 1024) >> 1);
-		
+
 		width = height = 1024;
-		
+
 		PANEL.setBounds(offsetX, offsetY, width, height);
 		this.FRAME.getContentPane().add(PANEL);
+
+		BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+		Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
+		this.FRAME.getContentPane().setCursor(blankCursor);
 
 		this.FRAME.revalidate();
 	}
@@ -41,7 +47,7 @@ public class MyWindow implements Window {
 	public JPanel getJPanel() {
 		return this.PANEL;
 	}
-	
+
 	public static Window getInstance() {
 		return INSTANCE;
 	}
@@ -50,6 +56,5 @@ public class MyWindow implements Window {
 	public Component getJFrame() {
 		return this.FRAME;
 	}
-	
-	
+
 }
