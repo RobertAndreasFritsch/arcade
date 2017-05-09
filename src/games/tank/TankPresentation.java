f01e982eb2931df1c71216671eec6734eeab53ac
Felix Thalmann <felix.thalmann@online.de>
2017-05-09 08:43:03 +0200

    Tank Presesentation
    Bild hinzufügt

--- src/games/tank/TankPresentation.java
+++ src/games/tank/TankPresentation.java
@@ -1,5 +1,10 @@
 //package games.tank;
 //
+//import java.awt.Graphics2D;
+//import java.awt.Image;
+//import java.awt.Toolkit;
+//import java.io.IOException;
+//
 //import javax.swing.JPanel;
 //
 //import environment.launch.Presentation;
@@ -8,9 +13,23 @@
 //
 //public class TankPresentation implements Presentation {
 //
-//	@Override
-//	public Game getGame(JPanel panel, KeyRequest KEYS) {
-//		return new _Tank(panel, KEYS);
+//	Image img;
+//
+//	/**
+//	 * @throws IOException
+//	 */
+//	public TankPresentation() {
+//		img = Toolkit.getDefaultToolkit().getImage("res/games/tank/img/tank_Presesentation.png");
 //	}
 //
+//	@Override
+//	public void draw(Graphics2D g) {
+//		g.drawImage(img, 0, 0, 1024,1024, null);
+//	}
+//	
+//	
+//	@Override
+//	public Game getGame(JPanel panel, KeyRequest KEYS) {
+//		return new Tank(panel, KEYS);
+//	}
 //}
