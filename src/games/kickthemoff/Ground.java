8fe628af971797b9838110cbf190088040848042
TomKiesow <tomkiesow123@gmail.com>
2017-05-16 09:03:49 +0200

    the ground in kickthemoff will get smaller the longer ou play

--- src/games/kickthemoff/Ground.java
+++ src/games/kickthemoff/Ground.java
@@ -3,13 +3,16 @@ package games.kickthemoff;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.geom.Ellipse2D;
 
 import environment.model.gameobject.Drawable;
+import environment.model.gameobject.Updateable;
 
-public class Ground implements Drawable {
+public class Ground implements Drawable, Updateable {
 	private Image tex = Toolkit.getDefaultToolkit().createImage("res/games/kickthemoff/platform.png");
-	private int x, y, r;
-	private float size = 100;
+	private int x, y;
+	private float r;
+	private float SIZE_DECREASE_PER_SEC = 5;
 
 	public Ground(int x, int y, int r) {
 		this.x = x;
@@ -19,14 +22,16 @@ public class Ground implements Drawable {
 
 	@Override
 	public void draw(Graphics2D g) {
-		g.drawImage(tex, x - r, y - r, x + r, y + r, 0, 0, tex.getWidth(null), tex.getHeight(null), null);
+		g.setClip(new Ellipse2D.Float(x - r, y - r, 2 * r, 2 * r));
+		g.drawImage(tex, 0, 0, null);
+		g.setClip(null);
 	}
 
-	public float getSize(){
-		return size;
+	public void setR(int r) {
+		this.r = r;
 	}
-	
-	public int getR() {
+
+	public float getR() {
 		return r;
 	}
 
@@ -36,6 +41,13 @@ public class Ground implements Drawable {
 
 	public int getY() {
 		return y;
+	}
+
+	@Override
+	public void update(long elapsed) {
+		float factor = (float) elapsed / 1000;
+		factor = SIZE_DECREASE_PER_SEC * factor;
+		r -= factor;
 	}
 
 }
